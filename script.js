92ca9a43d3d9ad618647c7ccaf2920071a935bfc
eunicejihyun <eunicejihyun@gmail.com>
2022-11-20 20:18:27 -0500

    update footer

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ today = new Date();
 let copyright_year = document.getElementById('year');
 
 copyright_year.textContent=today.getFullYear();
+
+$(function () {
+    var includes = $('[include]')
+    $.each(includes, function () {
+      var file = 'views/' + $(this).data('include') + '.html'
+      $(this).load(file)
+    })
+  })
